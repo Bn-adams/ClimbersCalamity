03f614636eca9bb2d3d910ed9a3be1bfcd49cfb3
zzzack77 <125263916+zzzack77@users.noreply.github.com>
2024-03-06 19:49:47 +0000

    Added P1 stats and movmenet

--- ClimbersCalamity/Assets/Scripts/CharacterScripts/P2Mov.cs
+++ ClimbersCalamity/Assets/Scripts/CharacterScripts/P2Mov.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class P2Mov : MonoBehaviour
 {
-    public P1Mov p1Mov;
-    public P2Mov p2Mov;
-    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        p1Mov = player.GetComponent<P1Mov>();
+        
     }
 
     // Update is called once per frame
