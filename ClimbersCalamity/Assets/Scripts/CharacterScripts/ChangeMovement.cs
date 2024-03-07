3e486ae5f1f9d8a9f703ff872c9b3794cf3dc389
KLDest <117675213+KLDest@users.noreply.github.com>
2024-03-07 19:38:33 +0000

    Got the button UI and input working now need to add Functions to them

--- ClimbersCalamity/Assets/Scripts/CharacterScripts/ChangeMovement.cs
+++ ClimbersCalamity/Assets/Scripts/CharacterScripts/ChangeMovement.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ChangeMovement : MonoBehaviour
 {
     [Header("Players references and scripts")]
     public Collider2D playerCollider;
     public GameObject Checkpoint;
-    public P1Mov player1Move; // player 1 movement script
+    
     public GameObject playerObject; // player object
 
     [Header("UI")]
     public GameObject panelPopUP;
+    public Image FillerImageE;
+    public Image FillerImageR;
+    public Image FillerImageT;
+    public Image FillerImageY;
     //public Panel filler;
 
     [Header("Vectors")]
@@ -20,34 +26,87 @@ public class ChangeMovement : MonoBehaviour
 
     [Header("Inputs")]
     public KeyCode E;
+    public KeyCode R;
+    public KeyCode T;
+    public KeyCode Y;
+
+    [Header("Countdown")]
+    public float UICountdownE = 2f;
+    public float UICountdownR = 2f;
+    public float UICountdownT = 2f;
+    public float UICountdownY = 2f;
+
+    [Header("Bools")]
+    public bool isPanelPopUPActive;
 
     // Start is called before the first frame update
     void Start()
     {
         panelPopUP.SetActive(false);
         UIOffset = new Vector3(-1, 1, 0);
+        FillerImageE.fillAmount = 0;
+        FillerImageR.fillAmount = 0;
+        FillerImageT.fillAmount = 0;
+        FillerImageY.fillAmount = 0;
+        isPanelPopUPActive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        checkpointFiller();
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.name == "P1") // can probably change this later to a tag so that player 1 and player 2 have the same player tag and its more efficient
         {
             panelPopUP.SetActive(true);
             panelPopUP.transform.position = Checkpoint.transform.position + UIOffset;
+            isPanelPopUPActive = true;
             //player1Move.enabled = false; USE THIS LATER TO SWITCH SCRIPTS
-            Debug.Log("collision with player");
         }
-        if (Input.GetKey(E))
-        {
-
-        }
-
-
+        
     }
+    public void checkpointFiller()
+    {
+        if (isPanelPopUPActive == true && Input.GetKey(E))
+        {
+            UICountdownE -= Time.deltaTime;
+            FillerImageE.fillAmount += 2 * Time.deltaTime;
+        }
+        else FillerImageE.fillAmount -= 2 * Time.deltaTime;
+
+
+        if (isPanelPopUPActive == true && Input.GetKey(R))
+        {
+            UICountdownR -= Time.deltaTime;
+            FillerImageR.fillAmount += 2 * Time.deltaTime;
+        }
+        else FillerImageR.fillAmount -= 2 * Time.deltaTime;
+
+
+        if (isPanelPopUPActive == true && Input.GetKey(T))
+        {
+            UICountdownT -= Time.deltaTime;
+            FillerImageT.fillAmount += 2 * Time.deltaTime;
+        }
+        else FillerImageT.fillAmount -= 2 * Time.deltaTime;
+
+
+        if (isPanelPopUPActive == true && Input.GetKey(Y))
+        {
+            UICountdownY -= Time.deltaTime;
+            FillerImageY.fillAmount += 2 * Time.deltaTime;
+        }
+        else FillerImageY.fillAmount -= 2 * Time.deltaTime;
+    }
+
+    
 }
